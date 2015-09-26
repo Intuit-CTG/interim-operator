962d73b07cb056462362fe59cab527db15ca9b4a
Leo Wu <leowu@airbears2-10-142-39-31.AirBears2.1918.Berkeley.EDU>
2015-09-26 09:48:45 -0700

    fixed this fucking problem

--- extension/div_adder.js
+++ extension/div_adder.js
@@ -1,4 +1,10 @@
-var sections = document.getElementsByClassName('zsg-content-section collapsible');
+function handleResult(str) {
+    var sections = document.getElementsByClassName('zsg-content-section collapsible');
+    sections[0].insertAdjacentHTML('afterEnd', '<section class="zsg-content-section collapsible collapsed"><h2 class=<collapsible-header">Tax Effects</h2><div>' + str + '</div></section>');
+}
 
-sections[0].insertAdjacentHTML('afterEnd', '<section class="zsg-content-section collapsible collapsed"><h2 class=<collapsible-header">Tax Effects</h2><div>FUCK YOU</div></section>');
+var cost = $("div.main-row.home-summary-row").find("span").html().split('<')[0]
+var value = cost.split("$")[1].split(",").join("");
+var state = $("span.zsg-h2.addr_city").html().split(", ")[1].split(" ")[0]
 
+handleResult(taxCalc(state, value))
