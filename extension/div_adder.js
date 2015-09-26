214175c2881a77f0871e1a3dab25b337ac4c6024
Megan Zhu <meganz@berkeley.edu>
2015-09-26 05:05:17 -0700

    pooooooop

--- extension/div_adder.js
+++ extension/div_adder.js
@@ -2,3 +2,5 @@ if (document.title.indexOf("Google") != -1) {
     var test = document.getElementById("lga");
     test.innerHTML = '<font size="72">Congratulations, buttwipe!</font>';
 }
+
+function 
